99dae933f7de0508a831d2a5581f69587a30b1a4
Ben Durrant <ben.j.durrant@gmail.com>
2023-05-08 12:06:21 +0100

    initial setup

---
+++ index.ts
@@ -0,0 +1,14 @@
+type IfMaybeUndefined<T, True, False> = [undefined] extends [T] ? True : False;
+
+const testAccess = ({} as Record<string, 0>).a;
+
+export type IfUncheckedIndexedAccess<True, False> = IfMaybeUndefined<
+  typeof testAccess,
+  True,
+  False
+>;
+
+export type UncheckedIndexedAccess<T> = IfUncheckedIndexedAccess<
+  T | undefined,
+  T
+>;
